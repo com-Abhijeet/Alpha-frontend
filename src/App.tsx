7ceaa133e9e32e4d42a2a8ed5a2a50b772ea92c2
Abhijeet <shindeabhijeet552@gmail.com>
2024-08-13 14:32:00 +0530

    google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js";
 
-import Analytics from "./middleware/googleAnalytics";
 import Landing from "./pages/Landing";
 import useMuteMedia from "./helpers/useMuteMedia";
 
-// import ReactGA from "react-ga";
-// const TRACKING_ID = "G-LTC9GWLF3P"; // OUR_TRACKING_ID
-// ReactGA.initialize(TRACKING_ID);
+import ReactGA from 'react-ga4';
+ReactGA.initialize('G-LTC9GWLF3P');
+
 
 // Lazy load the Portfolio component
 const Portfolio = lazy(() => import("./components/Portfolio"));
@@ -20,11 +19,11 @@ const Portfolio = lazy(() => import("./components/Portfolio"));
 
 function App() {
   useMuteMedia();
-  Analytics();
-  // useEffect(() => {
-  //   // Track page view
-  //   ReactGA.pageview(window.location.pathname + window.location.search);
-  // }, []);
+  ReactGA.send({
+    hitType : "pageview",
+    page : "/",
+    title : "Home Page"
+  });
   
   return (
     <>
