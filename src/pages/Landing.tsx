1343a8bc3f8cf196785ce699e04a19bc448bfce6
Abhijeet <shindeabhijeet552@gmail.com>
2024-08-15 19:42:16 +0530

    Added Chatbot

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -9,8 +9,19 @@ import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import Clients from "../components/Clients";
 import Portfolio from "../components/Portfolio";
+import { useEffect } from "react";
 
 const Landing = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "//code.tidio.co/2yg02nazvhxb1e8rx1hz7k57wh83quzs.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <>
       <Navbar />
